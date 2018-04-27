265fc1dcf2aefe57cdec1b10a6f9a3c7267b4d0a
Ming Yi <mingyiteo@gmail.com>
2018-04-26 19:42:06 -0700

    Add NutritionIX barcode lookup API

--- NutritionIX.py
+++ NutritionIX.py
@@ -6,8 +6,10 @@ NUTRITION ID REFERENCE:
 https://docs.google.com/spreadsheets/d/14ssR3_vFYrVAidDLJoio07guZM80SMR5nxdGpAX-1-A/edit#gid=0
 """
 
-import requests
 import json
+from PIL import Image
+from pyzbar.pyzbar import decode
+import requests
 
 
 APPLICATION_ID = '08a406db'
@@ -96,7 +98,7 @@ def restaurant_lookup(measure='point_distance'):
     Supports point distance or bounding box queries
     Reference: https://gist.github.com/SorJEF/6319e7f6a0474a8a2b2ada80037fffe7
     """
-    url = ' https://trackapi.nutritionix.com/v2/locations'
+    url = 'https://trackapi.nutritionix.com/v2/locations'
 
     # Lookup by distance from point (lon, lat)
     if measure == 'point_distance':
@@ -123,8 +125,25 @@ def restaurant_lookup(measure='point_distance'):
 
 def barcode_lookup():
     """
-    Coming soon
+    Nutrition lookup using barcode
+    Reference: https://gist.github.com/mattsilv/478c9288f213ce5333399a41bd6da5a4
     """
+    url = 'https://trackapi.nutritionix.com/v2/search/item'
+    params = {
+        'upc': decode_barcode()
+    }
+    response = requests.get(url=url, headers=HEADERS, params=params)
+    content = json.loads(response.content)
+
+    print(json.dumps(content, indent=4))
+
+
+def decode_barcode():
+    """
+    Decodes barcode using pyzbar library
+    """
+    decoded_barcode = decode(Image.open('sample-barcode.png'))
+    return decoded_barcode[0][0]
 
 
 # quick_food_search()
@@ -132,4 +151,5 @@ def barcode_lookup():
 # filtered_search()
 # restaurant_lookup(measure='point_distance')
 # restaurant_lookup(measure='bounded_box')
+# decode_barcode()
 barcode_lookup()
